bfb558a2dd186d7f97a3427423ebbbda53626ab0
jagadish <jagadish.dhandapani-extern@rntbci.com>
2022-10-04 00:41:13 +0530

    onboarding process

--- onboard/src/components/Form.js
+++ onboard/src/components/Form.js
@@ -3,7 +3,7 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
-import Form1 from "./Form1.css"
+import "./Form.css"
 import Stepper from "react-stepper-horizontal";
 
 
